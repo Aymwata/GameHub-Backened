17247b894a52ab77ff9f7b3511a960fb637b37d4
Aymwata <ma.naranjoo@duocuc.cl>
2026-06-19 22:16:19 -0400

    Documentqacion Swagger y config++

---
+++ category-service/src/main/java/com/gamehub/category_service/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.gamehub.authservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Auth Service API - GameHub")
+                        .version("1.0.0")
+                        .description("Documentación interactiva para el microservicio de autenticación."));
+    }
+}
