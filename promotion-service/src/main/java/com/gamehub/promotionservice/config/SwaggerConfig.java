3bf1efe25cdac6c71c877265a03038ffe82963de
Maximiliano Isamit <ma.isamit@duocuc.cl>
2026-06-22 20:10:59 -0400

    configuracion y arreglo de swagger

---
+++ promotion-service/src/main/java/com/gamehub/promotionservice/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.gamehub.promotionservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Promotion Service API - GameHub")
+                        .version("1.0.0")
+                        .description("Documentación interactiva para el motor de promociones y cupones de descuento."));
+    }
+}
